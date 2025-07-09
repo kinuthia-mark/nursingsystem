29c43b4e2245ae7585518dcdd6f1d204fe22bffb
Davidchuck <rawlencendirangu@gmail.com>
2025-07-09 16:39:11 +0300

    Admin Dashboard

---
+++ index.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <?php include 'head.php';  ?>
+  <body class="app sidebar-mini">
+    <?php include 'sidenav.php';  ?>    
+    <main class="app-content">
+      <div class="app-title">
+        <div>
+          <h1><i class="fa fa-dashboard"></i> Nursing Database Management System </h1>        
+        </div>       
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-lg-3">
+          <div class="widget-small primary coloured-icon"><i class="icon fa fa-list fa-3x"></i>
+            <div class="info">
+              <h6>Registered Children</h6>
+              
+              <a href="master_list.php">
+              <p><b>                
+                
+              </b></p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
+            <div class="info">
+              <h6>Active Children</h6>
+              <a href="active_auctions.php">
+              <p><b>
+              
+                     
+              </b></p>
+                  </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="widget-small warning coloured-icon"><i class="icon fa fa-users fa-3x"></i>
+            <div class="info">
+              <h6>Conducted tests</h6>
+              <a href='all_users.php'>
+              <p><b>
+              
+              </b></p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="widget-small info coloured-icon"><i class="icon fa fa-users fa-3x"></i>
+            <div class="info">
+              <h6>Registered Users</h6>
+              <a href='companyReg.php'>
+              <p><b>
+             
+
+              </b></p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        
+      
+      </div>
+    </main>
+    <!-- Essential javascripts for application to work-->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="js/plugins/pace.min.js"></script>
+    <!-- Page specific javascripts-->
+    <script type="text/javascript" src="js/plugins/chart.js"></script>
+    <script type="text/javascript"></script>
+    
+  </body>
+</html>
---
+++ sidenav.php
@@ -0,0 +1,74 @@
+<!-- Navbar-->
+    <header class="app-header"><a class="app-header__logo" href="index.php" style="background-color:rgb(41, 129, 216); color: white; font-family:veltica; font-size:130%">N-DBMS</a>
+     <a class="app-sidebar__toggle" style="background-color:rgb(41, 129, 216);" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+      <!-- Navbar Right Menu-->
+      <ul class="app-nav" style="background-color:rgb(41, 129, 216); color: white;">
+        <li class="app-search">
+          <input class="app-search__input" type="search" placeholder="Search">
+          <button class="app-search__button"><i class="fa fa-search"></i></button>
+        </li>
+    
+        <!-- User Menu-->
+        <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu">
+          <i class="fa fa-user fa-lg"></i>
+        </a>
+          <ul class="dropdown-menu settings-menu dropdown-menu-right">
+           <li><a class="dropdown-item" href="page-user.php"><i class="fa fa-user fa-lg"></i> User Profile</a></li>
+           <li <?php echo "$parasithide $biochemhide $immunohaemat_hide $Virology_hide $Covid_hide"; ?> ><a class="dropdown-item" href="users.php"><i class="fa fa-cog fa-lg"></i> Manage Users</a></li>
+            <li><a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </header>
+
+    <!-- Sidebar menu-->
+    <aside class="app-sidebar" style="background-color:rgb(14, 30, 117); color: white;">
+      <div class="app-sidebar__user">
+        <img class="app-sidebar__user-avatar" src="images/user.png" width="20%" height="20%" alt="User Image">
+        <div>
+          <p class="app-sidebar__user-name">User:<?php ;?>      </p>
+          <p class="app-sidebar__user-designation">
+
+  
+            </p>
+        </div>
+      </div>
+
+      <ul class="app-menu">
+        <li><a class="app-menu__item active" href="index.php"><i class="app-menu__icon fa fa-dashboard">
+          
+        </i><span class="app-menu__label">Dashboard</span></a></li>
+
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-plus"></i><span class="app-menu__label">Register Management</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+
+            <li  ><a class="treeview-item" href="item_register.php"><i class="icon fa fa-circle-o"></i> Children Register</a></li> 
+          </ul>
+        </li>       
+
+           <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-list" ></i>
+
+            <span class="app-menu__label">Master List</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+              <ul class="treeview-menu">
+                <li ><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i>Registered Children</a></li>   
+                <li ><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i>ARTS</a></li>          
+              </ul>
+           </li>    
+
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Reports</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li><a class="treeview-item" href="#"><i class="icon fa fa-users"></i>Report 1</a></li>
+            <li><a class="treeview-item" href="#"><i class="icon fa fa-gear"></i>Report 2</a></li>           
+          </ul>
+        </li> 
+
+
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-wrench"></i><span class="app-menu__label">Settings</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+            <ul class="treeview-menu">
+              <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i>Company Registration</a></li>
+              <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i>Client's Admin Register</a></li>        
+            </ul>
+        </li>
+      </ul>
+    </aside>
+
