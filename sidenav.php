8ec0014be25c0cde03c89728c1b65f6b020d58ed
Davidchuck <rawlencendirangu@gmail.com>
2025-07-10 16:33:07 +0300

    Remove conditional visibility from Manage Users link in sidenav for consistent access.

--- sidenav.php
+++ sidenav.php
@@ -14,7 +14,7 @@
         </a>
           <ul class="dropdown-menu settings-menu dropdown-menu-right">
            <li><a class="dropdown-item" href="page-user.php"><i class="fa fa-user fa-lg"></i> User Profile</a></li>
-           <li <?php echo "$parasithide $biochemhide $immunohaemat_hide $Virology_hide $Covid_hide"; ?> ><a class="dropdown-item" href="users.php"><i class="fa fa-cog fa-lg"></i> Manage Users</a></li>
+           <li><a class="dropdown-item" href="users.php"><i class="fa fa-cog fa-lg"></i> Manage Users</a></li>
             <li><a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
           </ul>
         </li>
