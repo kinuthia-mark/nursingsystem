95eaededfea25c4d70b541b09c34a90f5cc7135f
Wickedwhip <joemwago1968@gmail.com>
2025-07-16 09:35:02 +0300

    reversed my previous dbname to nursing

--- dbconnect.php
+++ dbconnect.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "nursing_masterlist";
+$dbname = "nursing";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
